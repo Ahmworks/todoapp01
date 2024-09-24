e077cb7732f6e59ae5f62ec0fa71437f7d0cb500
Ahm <ahmwork35@gmail.com>
2024-09-24 18:22:09 +0300

    son

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -45,7 +45,7 @@ export const TodoWrapper = () => {
   };
   return (
     <div className="TodoWrapper">
-      <h1>Get Things Done</h1>
+      <h1>To Do List</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) =>
         todo.isEditing ? (
